c4a19784938fdf10acb7168f1ecc5b85ff4111f8
Adamou SALIFOU <ibnadam2010@yahoo.fr>
2023-08-25 12:11:35 +0200

    create new entity categorie

--- tests/UserUnitTest.php
+++ tests/UserUnitTest.php
@@ -10,11 +10,23 @@ use App\Entity\User;
 use PHPUnit\Framework\TestCase;
  
 class UserTest extends TestCase{
-public function testUserCreate(){
-$user = new User(); // Create User object.
-    $user->setNom("Bertin")
-         ->setPrenom("Alice")
-         ->setEmail("as.toto@gmail.com");
-    $this->assertTrue($user->getNom()==="Bertin");
-}
+
+    public function testUserCreate(){
+        $user = new User(); // Create User object.
+        $user->setNom("Name");
+         
+        $this->assertEquals("Name", $user->getNom());
+        }
+
+    public function testIsTrue(){
+
+        $user = new User();
+        $user->setNom("Bertin")
+            ->setPrenom("Alice")
+            ->setEmail("as.toto@gmail.com");
+        $this->assertTrue($user->getNom()==="Bertin");
+        }
+
+
+
 }
