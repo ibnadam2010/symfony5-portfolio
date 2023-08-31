81aca182f8bc24266f5628111c5603b9f83d2d15
Adamou SALIFOU <ibnadam2010@yahoo.fr>
2023-08-31 19:15:12 +0200

    update npm libraries and dependancies

---
+++ assets/js/theme.js
@@ -0,0 +1,20 @@
+import 'bootstrap';
+import 'flickity';
+import 'flickity-imagesloaded';
+import 'flickity-as-nav-for';
+import 'flickity-fade';
+import 'jarallax';
+import 'quill';
+import 'smooth-scroll';
+import 'navbar';
+import 'modal';
+import 'popover';
+import 'aos';
+
+
+
+import './bigpicture';
+import './choices';
+import './countup';
+import './jarallax';
+import './navbar-dropdown';
